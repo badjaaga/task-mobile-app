41e2e150a9b10785083257be882f5a549a8a582f
Nadzeya Molchan <nadzeya_molchan@epam.com>
2024-02-22 18:46:08 +0100

    refactor: cleanup

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,12 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true)
 
-  const presets = [
-    ['babel-preset-expo'],
-  ];
+  const presets = [['babel-preset-expo']]
 
-  const plugins = [
-    'react-native-paper/babel',
-    [
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env"
-      }
-    ]
-  ];
+  const plugins = ['react-native-paper/babel']
 
   return {
     presets,
     plugins,
-  };
-};
+  }
+}
